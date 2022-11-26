fc948c32441959bcfab410a79ea332e944bfdc10
Cam <cameron.miller294@gmail.com>
2022-11-26 17:55:35 -0500

    Update kanabox.js
    Removed unused code

--- client/src/components/kana/kanabox.js
+++ client/src/components/kana/kanabox.js
@@ -13,33 +13,8 @@ const KanaBox = ({ kana }) => {
   const handleClick = (event) => {
     const clickedKana = event.target.textContent;
     const theType = determineType(clickedKana);
-    // dispatch({ type: 'set prev query', payload: ''});
     dispatch({ type: theType, payload: clickedKana });
-    // getResults();
   }
-
-  // const searchDict = () => {
-  //   let matches = [];
-  //   let exs;
-  //
-  //   for (let i = 0; i < state.kanji.length; i++) {
-  //     for (let j = 0; j < state.kanji[i].examples.length; j++) {
-  //       exs = state.kanji[i].examples[j];
-  //       if (exs[1].includes(state.query)) {
-  //         matches.push([exs.indexOf(state.query), exs]);
-  //       }
-  //     }
-  //   }
-  //
-  //   // matches.sort((a, b) => { return compareResults(a, b) })
-  //
-  //   return matches;
-  // }
-  //
-  // const getResults = async () => {
-  //   const matches = await searchDict();
-  //   dispatch({ type: 'update matches', payload: matches });
-  // }
 
   return (
     <div className="kana-box" onClick={(e) => handleClick(e)}>
